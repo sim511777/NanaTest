6ff9e1c7c63b367fb677df36f445495188f773e0
sim511777 <sim51177@gmail.com>
2020-06-10 18:01:56 +0900

    클래스로 코딩

--- NanaTest/main.cpp
+++ NanaTest/main.cpp
@@ -2,29 +2,35 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
 
+using namespace nana;
+class MyForm : public form {
+private:
+    label lab;
+    button btn;
+    void InitializeComponents() {
+        lab.create(*this);
+        lab.caption("Hello, <bold blue size=16>Nana C++ Library</>");
+        lab.format(true);
+
+        btn.create(*this);
+        btn.caption("Quit");
+        btn.events().click([this] {
+            this->close();
+            });
+
+        this->div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
+        (*this)["text"] << lab;
+        (*this)["button"] << btn;
+        this->collocate();
+    }
+public:
+    MyForm() {
+        InitializeComponents();
+    }
+};
+
 int main() {
-    using namespace nana;
-
-    //Define a form.
-    form fm;
-
-    //Define a label and display a text.
-    label lab{ fm, "Hello, <bold blue size=16>Nana C++ Library</>" };
-    lab.format(true);
-
-    //Define a button and answer the click event.
-    button btn{ fm, "Quit" };
-    btn.events().click([&fm] {
-        fm.close();
-        });
-
-    //Layout management
-    fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
-    fm["text"] << lab;
-    fm["button"] << btn;
-    fm.collocate();
-
-    //Show the form
+    MyForm fm;
     fm.show();
 
     //Start to event loop process, it blocks until the form is closed.
